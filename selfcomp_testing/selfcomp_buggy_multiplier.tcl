1c9cdb87c983ccea5666df27f82404fb1edad0a4
unknown <smarina@princeton.edu>
2024-06-27 13:38:24 -0400

    added buggy multiplier test

---
+++ selfcomp_testing/selfcomp_buggy_multiplier.tcl
@@ -0,0 +1,15 @@
+analyze -sva selfcomp_buggy_multiplier.v
+
+elaborate -top BuggyMultiplierTester -bbox_mul 256
+
+#specify clock and reset
+clock clock
+reset reset -non_resettable_regs 0
+
+stopat se1.aes_invcipher.io_output_op1_0
+stopat se2.aes_invcipher.io_output_op1_0
+
+assert {timingLeakDone -> !timingLeak}
+# assert {bothValid -> io_out_resultOne == io_out_resultTwo}
+
+prove -bg -all
