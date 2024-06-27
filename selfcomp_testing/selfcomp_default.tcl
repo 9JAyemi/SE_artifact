8c32e8b48138890339a26da40bbfc002cec94b4c
Sofia Marina <sm8765@princeton.edu>
2024-06-26 23:14:33 -0400

    typo fix

--- selfcomp_testing/selfcomp_default.tcl
+++ selfcomp_testing/selfcomp_default.tcl
@@ -6,10 +6,10 @@ elaborate -top DefaultTester -bbox_mul 256
 clock clock
 reset reset -non_resettable_regs 0
 
-stopat se1.AESDecrypt.io_output_op1_0
-stopat se2.AESDecrypt.io_output_op1_0
+stopat se1.aes_invcipher.io_output_op1_0
+stopat se2.aes_invcipher.io_output_op1_0
 
 assert {timingLeakDone -> !timingLeak}
-assert {bothDone -> io_out_resultOne == io_out_resultTwo}
+assert {bothValid -> io_out_resultOne == io_out_resultTwo}
 
 prove -bg -all
