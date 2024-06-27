2f7c8a105c3c804637425185299c8d5b0bb6cbdb
Sofia Marina <sm8765@princeton.edu>
2024-06-27 14:58:54 -0400

    time limit

--- selfcomp_testing/selfcomp_buggy_multiplier.tcl
+++ selfcomp_testing/selfcomp_buggy_multiplier.tcl
@@ -12,4 +12,5 @@ stopat se2.aes_invcipher.io_output_op1_0
 assert {timingLeakDone -> !timingLeak}
 # assert {bothValid -> io_out_resultOne == io_out_resultTwo}
 
+set_prove_time_limit 3600
 prove -bg -all
